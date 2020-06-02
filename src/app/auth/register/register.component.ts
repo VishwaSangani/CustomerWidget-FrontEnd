62c8805d9aaa087111a78dbb9687af4cff7f8b64
bratinkundu <bkundu53@gmail.com>
2020-06-02 13:07:13 +0530

    Added form validation in register page

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators,FormBuilder } from '@angular/forms';
+import { FormGroup, AbstractControl, Validators,FormBuilder } from '@angular/forms';
 import { states } from '../../../environments/variables'
 
 
@@ -18,18 +18,21 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.states = states;
     this.registerForm = this.formBuilder.group({
-      firstName : ['',Validators.required],
-      lastName : ['',Validators.required],
+      firstName : ['',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]],
+      lastName : ['',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]],
       emailId : ['',[Validators.required,Validators.email]],
       address : ['',Validators.required],
       address2 : [''],
-      mobileNumber : ['',Validators.required],
+      mobileNumber : ['',[Validators.required,Validators.pattern("[0-9 ]{10}")]],
       city : ['',Validators.required],
       state : ['',Validators.required],
-      zipcode : ['',[Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
-      password : ['',Validators.required]
+      zipcode : ['',[Validators.required, Validators.minLength(6), Validators.maxLength(6), Validators.pattern("[0-9 ]{6}")]],
+      password : ['',Validators.required],
+      confirmPassword : ['', Validators.required],
     });
   }
+
+ 
 
   onSubmit(){
     alert('Clicked')
