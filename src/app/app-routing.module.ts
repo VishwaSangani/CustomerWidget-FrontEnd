3eed4d46002a540d6803993a932d6fcc1eaac459
bratinkundu <bkundu53@gmail.com>
2020-06-08 17:49:49 +0530

    Added the AuthGuard for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { AuthModule } from './auth/auth.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuard } from './shared/Guards/login.guard';
 
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
   },
   {
     path: 'booking',
-    loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule)
+    loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule),
+    canActivateChild : [LoginGuard]
   },
   {
     path: '**',
